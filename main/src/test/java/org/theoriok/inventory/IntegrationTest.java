7f81bc697d5d00b6da9fa4a8e47ace9ae69c45e1
Kevin <kevin.van.dyck@gmail.com>
2022-05-15 11:53:18 +0200

    fix build?

--- main/src/test/java/org/theoriok/inventory/IntegrationTest.java
+++ main/src/test/java/org/theoriok/inventory/IntegrationTest.java
@@ -2,8 +2,10 @@ package org.theoriok.inventory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IntegrationTest {
     @Test
     void shouldStartApplicationContext() {
