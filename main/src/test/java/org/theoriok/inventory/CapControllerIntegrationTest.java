d1370ae88a3aff498def99e72cf252ee49c961fb
Kevin <kevin.van.dyck@gmail.com>
2022-05-29 20:35:12 +0200

    failing test for find caps by country

--- main/src/test/java/org/theoriok/inventory/CapControllerIntegrationTest.java
+++ main/src/test/java/org/theoriok/inventory/CapControllerIntegrationTest.java
@@ -60,6 +60,21 @@ class CapControllerIntegrationTest {
     }
 
     @Test
+    void shouldReturnCapsByCountry() throws Exception {
+        var country = testCountry("BE");
+        countryRepository.save(country);
+        capRepository.save(testCap(country, "BE-1"));
+        var differentCountry = testCountry("NL");
+        countryRepository.save(differentCountry);
+        capRepository.save(testCap(differentCountry, "NL-2"));
+
+
+        mvc.perform(get("/caps/BE"))
+            .andExpect(status().isOk())
+            .andExpect(content().json(expectedJsonArray()));
+    }
+
+    @Test
     void shouldReturnNotFoundWhenCapNotFoundById() throws Exception {
         mvc.perform(get("/caps/BE-1"))
             .andExpect(status().isNotFound())
@@ -164,13 +179,21 @@ class CapControllerIntegrationTest {
     }
 
     private CapEntity testCap(CountryEntity country) {
-        return new CapEntity("BE-1", "Belgian Cap", "This is a Belgian Cap", 5, country);
+        return testCap(country, "BE-1");
+    }
+
+    private CapEntity testCap(CountryEntity country, String businessId) {
+        return new CapEntity(businessId, "Belgian Cap", "This is a Belgian Cap", 5, country);
     }
     private CapEntity testCapWithSameIdButDifferentValues(CountryEntity country) {
         return new CapEntity("BE-1", "Jupiler", "Stella Artois en al", 1, country);
     }
 
     private CountryEntity testCountry() {
-        return new CountryEntity("Belgium", "BE");
+        return testCountry("BE");
+    }
+
+    private CountryEntity testCountry(String code) {
+        return new CountryEntity("Belgium", code);
     }
 }
