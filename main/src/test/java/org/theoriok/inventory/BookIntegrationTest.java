f7d9b9d2bfd09cb48dd589d8e4977edd12863f09
Kevin <kevin.van.dyck@gmail.com>
2022-07-03 20:33:59 +0200

    upsert book failing test

--- main/src/test/java/org/theoriok/inventory/BookIntegrationTest.java
+++ main/src/test/java/org/theoriok/inventory/BookIntegrationTest.java
@@ -1,6 +1,10 @@
 package org.theoriok.inventory;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.from;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -50,6 +54,21 @@ class BookIntegrationTest {
             .andExpect(content().json(expectedJsonArray()));
     }
 
+    @Test
+    void shouldInsertNewBook() throws Exception {
+        mvc.perform(put("/books")
+                .contentType(APPLICATION_JSON)
+                .content(bookToUpsert()))
+            .andExpect(status().isNoContent());
+
+        assertThat(bookRepository.findAll())
+            .singleElement()
+            .returns("BOOK-1", from(BookEntity::getBusinessId))
+            .returns("The Hobbit", from(BookEntity::getTitle))
+            .returns("JRR Tolkien", from(BookEntity::getAuthor))
+            .returns("In a hole under the ground, there lived a Hobbit.", from(BookEntity::getDescription));
+    }
+
     @NotNull
     private BookEntity testBook() {
         return new BookEntity("BOOK-1", "The Hobbit", "JRR Tolkien", "In a hole under the ground, there lived a Hobbit.");
@@ -66,6 +85,18 @@ class BookIntegrationTest {
                     "description": "In a hole under the ground, there lived a Hobbit."
                 }
             ]
+            """;
+    }
+
+    @Language("JSON")
+    private String bookToUpsert() {
+        return """
+            {
+                "business_id": "BOOK-1",
+                "title": "The Hobbit",
+                "author": "JRR Tolkien",
+                "description": "In a hole under the ground, there lived a Hobbit."
+            }
             """;
     }
 }
