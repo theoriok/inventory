25bf1de28c5cb59235097f1a18d646bde4c61d93
Kevin <kevin.van.dyck@gmail.com>
2022-07-21 20:36:15 +0200

    for now, sorting on name should be enough to reduce flakiness

--- main/src/test/java/org/theoriok/inventory/IntegrationTest.java
+++ main/src/test/java/org/theoriok/inventory/IntegrationTest.java
@@ -1,5 +1,7 @@
 package org.theoriok.inventory;
 
+import static java.util.Comparator.comparing;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -26,6 +28,8 @@ abstract class IntegrationTest {
 
     @BeforeEach
     void setUp() {
-        repositories.forEach(CrudRepository::deleteAll);
+        repositories.stream()
+            .sorted(comparing(jpaRepository -> jpaRepository.getClass().getSimpleName()))
+            .forEach(CrudRepository::deleteAll);
     }
 }
