00416911abf14388b8cddf628cad8e92223157f7
Kevin <kevin.van.dyck@gmail.com>
2022-07-26 17:11:51 +0200

    checkstyle

--- application/api/src/main/java/org/theoriok/inventory/command/DeleteBook.java
+++ application/api/src/main/java/org/theoriok/inventory/command/DeleteBook.java
@@ -2,6 +2,7 @@ package org.theoriok.inventory.command;
 
 public interface DeleteBook {
     Result delete(String businessId);
+
     enum Result {
         DELETED,
         NOT_FOUND
--- application/api/src/main/java/org/theoriok/inventory/command/DeleteCap.java
+++ application/api/src/main/java/org/theoriok/inventory/command/DeleteCap.java
@@ -2,6 +2,7 @@ package org.theoriok.inventory.command;
 
 public interface DeleteCap {
     Result delete(String businessId);
+
     enum Result {
         DELETED,
         NOT_FOUND
