b572c281e8f4f9b351a4732976c69dd84b506dc7
Kevin <kevin.van.dyck@gmail.com>
2023-10-08 14:13:46 +0200

    remove unused exception

--- application/api/src/main/java/org/theoriok/inventory/exception/UnknownCountryException.java
+++
@@ -1,7 +0,0 @@
-package org.theoriok.inventory.exception;
-
-public class UnknownCountryException extends RuntimeException {
-    public UnknownCountryException(String code) {
-        super("Unknown country %s".formatted(code));
-    }
-}
