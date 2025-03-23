bd972e70e9cdbb25f6d67b514d6bdde4cf3a40dd
Kevin Van Dyck <kevin.van.dyck@gmail.com>
2025-03-23 16:11:54 +0100

    make api more explicit

--- fe/inventory-frontend/src/api/book.api.ts
+++ fe/inventory-frontend/src/api/book.api.ts
@@ -1,13 +1,14 @@
 import {Book, BookApi, ListResponse} from "./book.api.types.ts";
 import {baseApi} from './base.api.ts';
+import {AxiosResponse} from "axios";
 
 export const bookApi: BookApi = {
     async fetchBook(business_id: string): Promise<Book> {
-        return baseApi.get(`books/${business_id}`)
+        return baseApi.get(`/books/${business_id}`)
     },
 
     async fetchBooks(): Promise<ListResponse<Book>> {
-        const {data} = await baseApi.get('books');
+        const {data}: AxiosResponse<Book[]> = await baseApi.get('/books');
         return {items: data, total: data.length};
     }
 }
