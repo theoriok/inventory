b461e4884927362cbaf4840bf09c0a3b33fd5010
kevin <kevin.van.dyck@gmail.com>
2025-09-07 14:44:41 +0200

    fix restart script

--- be/docker/restart.sh
+++ be/docker/restart.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-docker-compose down --remove-orphans
-docker run --rm --privileged alpine hwclock -s
-cleanup_images
-docker-compose up
 
 function cleanup_images() {
   if [ ! -z "$(docker images -f 'dangling=true' -q)" ]; then
      docker image prune -f
   fi
 }
+
+docker-compose down --remove-orphans
+docker run --rm --privileged alpine hwclock -s
+cleanup_images
+docker-compose up
