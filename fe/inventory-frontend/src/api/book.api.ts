730608205288e3f7030c9e76cb451a0e0c50fee6
Kevin Van Dyck <kevin.van.dyck@gmail.com>
2026-03-15 21:07:24 +0100

    implement create book on api

--- fe/inventory-frontend/src/api/book.api.ts
+++ fe/inventory-frontend/src/api/book.api.ts
@@ -1,4 +1,4 @@
-import {Book, BookApi, ListResponse} from "./book.api.types.ts";
+import {Book, BookApi, CreateBook, ListResponse} from "./book.api.types.ts";
 import {baseApi} from './base.api.ts';
 import {AxiosResponse} from "axios";
 
@@ -11,5 +11,10 @@ export const bookApi: BookApi = {
     async fetchBooks(): Promise<ListResponse<Book>> {
         const {data: books}: AxiosResponse<Book[]> = await baseApi.get('/books');
         return {items: books, total: books.length};
+    },
+
+    async createBook(book: CreateBook): Promise<Book> {
+        const {data: createdBook}: AxiosResponse<Book> = await baseApi.post('/books', book);
+        return createdBook;
     }
 }
