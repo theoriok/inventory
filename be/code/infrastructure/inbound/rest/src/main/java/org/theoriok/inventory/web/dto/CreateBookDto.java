aefcf456bbefe4e4cf9ba98d1447f59b201424c1
Kevin Van Dyck <kevin.van.dyck@gmail.com>
2026-04-18 16:57:53 +0200

    remove unneeded annotation

--- be/code/infrastructure/inbound/rest/src/main/java/org/theoriok/inventory/web/dto/CreateBookDto.java
+++ be/code/infrastructure/inbound/rest/src/main/java/org/theoriok/inventory/web/dto/CreateBookDto.java
@@ -3,9 +3,7 @@ package org.theoriok.inventory.web.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import org.springframework.validation.annotation.Validated;
 
-@Validated
 public record CreateBookDto(
     @JsonProperty("title") @NotBlank @Size(max = 255) String title,
     @JsonProperty("author") @NotBlank @Size(max = 255) String author,
--- be/code/infrastructure/inbound/rest/src/main/java/org/theoriok/inventory/web/dto/UpdateCapDto.java
+++ be/code/infrastructure/inbound/rest/src/main/java/org/theoriok/inventory/web/dto/UpdateCapDto.java
@@ -5,9 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
-import org.springframework.validation.annotation.Validated;
 
-@Validated
 public record UpdateCapDto(
     @JsonProperty("name") @NotBlank @Size(max = 255) String name,
     @JsonProperty("description") @NotBlank @Size(max = 5000) String description,
