6166f9dead0c87703024fd7dd38978e50234f9fa
Kevin Van Dyck <kevin.van.dyck@gmail.com>
2026-04-18 15:16:53 +0200

    Restrict allowed headers in CorsConfig

--- be/code/infrastructure/inbound/rest/src/main/java/org/theoriok/inventory/web/config/CorsConfig.java
+++ be/code/infrastructure/inbound/rest/src/main/java/org/theoriok/inventory/web/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins(allowedOrigins)
-            .allowedMethods("GET", "POST", "PUT", "DELETE");
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Accept", "Content-Type");
     }
 }
